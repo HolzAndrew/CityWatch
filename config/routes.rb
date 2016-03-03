67de5fe05ab3bb4d81e3abebf9f9ec36c312b432
Andrew Holz <Holz.Andrew@gmail.com>
2016-03-02 22:07:28 -0500

    /login

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ end
   
 root 'cities#index'
 
-post   'login'  => 'sessions#create'
+post   '/login'  => 'sessions#create'
 delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
